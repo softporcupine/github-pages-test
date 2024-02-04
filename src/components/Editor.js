659ddeb0905840817f0ce5de898757ad17486da9
stephen <stephen>
2024-02-04 11:05:59 -0800

    Add blank editor page with nav button

---
+++ src/components/Editor.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Editor = () => {
+    return (
+        <h1>Editor</h1>
+    )
+}
+
+export default Editor
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,16 +2,40 @@ import React, { useState } from "react";
 import Stopwatch from "./Stopwatch";
 import Talker from "./Talker";
 import ScreenWakeLock from "./ScreenWakeLock";
+import Editor from "./Editor";
 
 const Parent = () => {
     const [elapsedTime, setElapsedTime] = useState(0);
+    const [showEditor, setShowEditor] = useState(false);
+
+    function handleButton() {
+        setShowEditor(prevVal => !prevVal)
+    }
+
+    let mainScreen;
+    let buttonText = "Back"
+    if (showEditor) {
+        mainScreen =
+            <>
+                <Stopwatch onTimeChange={setElapsedTime} />
+                <Talker elapsedTime={elapsedTime} />
+                <ScreenWakeLock />
+            </>
+        buttonText = "Edit speech"
+    } else {
+        mainScreen =
+            <>
+                <Editor />
+            </>
+    }
 
     return (
-        <main>
-            <Stopwatch onTimeChange={setElapsedTime} />
-            <Talker elapsedTime={elapsedTime} />
-            <ScreenWakeLock />
-        </main>
+        <>
+            <button className="parent--nav-button" onClick={handleButton}>{buttonText}</button>
+            <main>
+                {mainScreen}
+            </main>
+        </>
     )
 }
 
